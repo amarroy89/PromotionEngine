86287f203a574c0c40c9511478a9b13e099fb5e6
Amar Roy <Amarnath.Roy@in.ey.com>
2020-07-05 17:49:00 +0530

    Add project files.

---
+++ PromotionEngineDAL/PromotionContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngineDAL
+{
+    public class PromotionContext : DbContext
+    {
+        //public PromotionContext(DbContextOptions options) : base(options)
+        //{
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+       => options.UseSqlite(@"Data Source=C:\Amar Doc\PromotionDB.db");
+        //C:\Users\Amarnath.Roy\Desktop\PromotionEngine-master\PromotionEngine-master\PromotionEngineDAL\Database
+        //(@"Data Source=D:\Shadab\Maersk\PromotionEngine\PromotionEngineDAL\Database\PromotionDB.db");
+        public DbSet<Sku> Skus { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+    }
+}
